19f579b0e1754d6ec4ca627d00f456d4e94449f8
Siddhesh Ghadi <30323015+Sid072504@users.noreply.github.com>
2021-06-03 17:15:44 +0530

    updated code

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ var imges = ['320X480V2/TELA%201.png',
 
 var output = document.getElementById('output');
 
-// Click count of Ads
-var clickcount = 0;
-
 //Clicking Set on Image2
 var clickset = 0
 var bigbtn = 0
@@ -36,8 +33,7 @@ function openfile(filepath){
 
 // click count but it refreshes again as per page loads
 function clicksnum(){
-            clickcount +=1;
-            //console.log("Clickcount = ", clickcount);
+            console.log("Clicked Now !!!!!");
 };
 
 function imgoff(){
@@ -51,7 +47,6 @@ catch(e){
         }
 };
 
-
 function btnsOff(){
 var btns = document.getElementsByTagName("button");
 for (i=0; i<btns.length; i++){
@@ -61,6 +56,10 @@ for (i=0; i<btns.length; i++){
 };
 };
 
+function multiplebtns(id){
+var btn = document.getElementById(id);
+btn.className = "selected";
+};
 
 function btnfunc(op){
     var img= document.getElementById("back");
@@ -68,29 +67,33 @@ function btnfunc(op){
     img.style= "height: 30px; width: 77px; position: absolute; top: 30px;left: 36px;";
     img.onclick = function(){btnsOff(); imgoff();op.src=imges[0]; };
 
-    var btn = document.getElementById("left1");
-    btn.style = "position: absolute; top: 260px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    var left1 = document.getElementById("left1");
+    left1.style = "position: absolute; top: 260px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    left1.onclick = function(){multiplebtns("left1");};
 
-    var btn = document.getElementById("right1");
-    btn.style = "position: absolute; top: 260px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    var right1 = document.getElementById("right1");
+    right1.style = "position: absolute; top: 260px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    right1.onclick = function(){multiplebtns("right1");};
 
-    var btn = document.getElementById("left2");
-    btn.style = "position: absolute; top: 324px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    var left2 = document.getElementById("left2");
+    left2.style = "position: absolute; top: 324px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    left2.onclick = function(){multiplebtns("left2");};
 
-    var btn = document.getElementById("right2");
-    btn.style = "position: absolute; top: 324px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    var right2 = document.getElementById("right2");
+    right2.style = "position: absolute; top: 324px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
+    right2.onclick = function(){multiplebtns("right2");};
 
     var left3 = document.getElementById("left3");
     left3.style = "position: absolute; top: 386px; left: 33px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
-    left3.onclick = function(){clickset = 3;};
+    left3.onclick = function(){clickset = 3; multiplebtns("left3");};
 
     var right3 = document.getElementById("right3");
     right3.style = "position: absolute; top: 386px; left: 167px; height: 50px; background: transparent; width: 135px; border-radius: 2%;border:1px solid transparent";
-    right3.onclick = function(){clickset = 5;};
+    right3.onclick = function(){clickset = 5; multiplebtns("right3");};
 
     var bigbtn = document.getElementById("bigbtn");
     bigbtn.style = "position: absolute; top: 475px; left: 52px; height: 65px; background: transparent; width: 229px; border-radius: 2%; border:1px solid transparent";
-    bigbtn.onclick = function(){ btnsOff(); imgoff(); bigbtn = 4; if (clickset==3){op.src = imges[2]; } else if (clickset==5){op.src = imges[4];} else op.src = imges[3];};
+    bigbtn.onclick = function(){btnsOff(); imgoff(); bigbtn = 4; if (clickset==3){op.src = imges[2]; } else if (clickset==5){op.src = imges[4];} else {op.src = imges[3];}};
 }
 
 
